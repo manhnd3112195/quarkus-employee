70647dd4a607885af53afc8a2dd68baf4cf66540
nguyenducmanh <nguyenducmanhbg@gmail.com>
2023-08-04 17:00:29 +0700

    update file pom and properties

--- src/test/java/com/techgeeknext/controller/EmployeeControllerIT.java
+++
@@ -1,8 +0,0 @@
-package com.techgeeknext.controller;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class EmployeeControllerIT extends EmployeeControllerTest {
-    // Execute the same tests but in packaged mode.
-}
